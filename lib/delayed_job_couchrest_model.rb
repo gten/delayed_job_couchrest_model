b8070b6cbac3f2c192b8015e44f03feeab895985
Geethan <geetan123321@gmail.com>
2011-10-24 20:13:05 +0530

    Correcting filename

--- lib/delayed_job_couchrest_model.rb
+++ lib/delayed_job_couchrest_model.rb
@@ -1,6 +1,6 @@
 require 'couchrest_model'
 require 'delayed_job'
-require 'delayed/serialization/couch_rest'
+require 'delayed/serialization/couchrest_model'
 require 'delayed/backend/couchrest_model'
 
 Delayed::Worker.backend = :couchrest_model
